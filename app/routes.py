efca5de2a2b31f3f0414dc535430f1f1f1e8d9f9
rijalul <rijalul@ecampus.ut.ac.id>
2020-09-10 11:05:25 +0700

    first commit

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+from app.controllers import controllerRoute
+
+@app.route('/ctr')
+def testctr():
+    return controllerRoute.fungsiController()
+@app.route('/ctr/')
+@app.route('/ctr/<parameter>')
+def testctParameter(parameter="kosong"):
+    return controllerRoute.fungsiControllerParameter(parameter)
+
+@app.route('/')
+def index():    #routes diatas, membaca satu def ini
+    return "Hello ,World!"
+
+ 
+@app.route('/users/')
+@app.route('/user/<name>') 
+@app.route('/user/')
+def tesParameter(name="kosong"): # 3 routes diatas, membaca satu def ini
+    return f"Hello,{name}!"
+
+@app.route('/users_coba/')
+@app.route('/users_coba/<name>')
+def tesParameter1(name="kosong"):# 2 routes diatas, membaca satu def ini
+    return f"coba,{name}!" 
